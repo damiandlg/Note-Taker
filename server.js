f7e9de8154fbcb793b983684ddd46777097a4c1a
damiandlg <damian.delagarzaf@gmail.com>
2022-11-04 21:14:24 -0500

    server.js almost done, still not working, have no fix notes.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require ("express");
 const fs = require ('fs');
 const path = require('path');
-require {notes} = require ('./db/db.json');
+const {notes} = require ('./db/db.json');
 const lib = require ('./notes')
 
 const app = express();
@@ -16,13 +16,34 @@ function newNote(body, messageArray){
   const note = body
   messageArray.push(note)
   fs.writeFileSync(
-    path.join(__dirname,'./db/db.son'),
+    path.join(__dirname,"db/db.json"),
     JSON.stringify({notes: messageArray}, null, 2),
   )
   return note
 }
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+
+app.get('/api/notes', (req, res) => {
+  let results = notes
+  if(req.query){
+    results = lib.filterByQuery(req.query, results)
+  }
+  res.json(results)
+})
+
+app.post('/api/notes', (req, res) => {
+  req.body.id = notes.length.toString()
+  const note = newNote(req.body, notes)
+  res.json(note)
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+})
+
 
 app.listen(PORT, () => {
     console.log("API server now on https;//localhost/" + PORT );
