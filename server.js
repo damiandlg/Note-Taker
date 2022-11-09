bee1f3d6e1021ca14c3c1f156cf9ababd7305fd1
damiandlg <damian.delagarzaf@gmail.com>
2022-11-08 20:51:21 -0600

    server working but still having 404 error

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require ("express");
 const fs = require ('fs');
 const path = require('path');
-const notes = require ('./db/db.json');
-const lib = require ('./lib/notes')
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+const notesList = require('./db/db.json')
 
 app.use(express.urlencoded({extended: true}));
 
@@ -15,16 +14,7 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-
-function createNewNote(body, messageArray){
-  const note = body
-  messageArray.push(note)
-  fs.writeFileSync(
-    path.join(__dirname, './db/db.json'),
-    JSON.stringify({notes: messageArray}, null, 2),
-  )
-  return note
-}
+//GET Function
 
 app.get('/api/notes', (req, res) => {
   let results = notes
@@ -32,21 +22,43 @@ app.get('/api/notes', (req, res) => {
     results = lib.filterByQuery(req.query, results)
   }
   res.json(results)
-})
-
-app.post('/api/notes', (req, res) => {
-  req.body.id = notes.length.toString()
-  const note = newNote(req.body, notes)
-  res.json(note)
-})
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'))
-})
+});
 
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'))
-})
+});
+
+// Create Note Function
+
+function createNewNote(body, messageArray){
+  const note = body;
+  if (!Array.isArray(notesArrayElements)) notesArrayElements = [];
+
+  if (notesArrayElements.length === 0) notesArrayElements.push(0);
+
+  body.id = notesArrayElements[0];
+  notesArrayElements[0]++;
+
+  notesArrayElements.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, "./db/db.json"),
+    JSON.stringify(notesArrayElements, null, 2)
+  );
+  return note;
+}
+
+//Post Function
+
+app.post('/api/notes', (req, res) => {
+  const note = newNote(req.body, notesList);
+  res.json(note);
+});
+
+
 
 
 app.listen(PORT, () => {
